73e349c0b345d140786fbfb42469c631eb3d88fa
valya_plotnikova <valya_ezhova@mail.ru>
2024-11-12 11:27:44 +0300

    change 5

--- cosmos/static/js/main.js
+++ cosmos/static/js/main.js
@@ -1,42 +1,45 @@
-Fancybox.bind('[data-fancybox="gallery"]', {
-
-});
-
-
-const el = document.querySelector(".test-clc");
-
-console.log('el', el)
-
-
-
-$(document).ready(function(){
+$(document).ready(function() {
     $('.slider-for').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
         arrows: false,
         fade: true,
         asNavFor: '.slider-nav',
-
-
     });
+
     $('.slider-nav').slick({
-        slidesToShow: 3,
+        slidesToShow: 5,
         slidesToScroll: 1,
         asNavFor: '.slider-for',
-        dots: true,
-        centerMode: true,
+        dots: false,
+//        centerMode: true,
         focusOnSelect: true,
-        variableWidth: true,
+        arrows: true,
+        prevArrow: '<div class="prev"></div>',
+        nextArrow: '<div class="next"></div>',
 
         responsive: [
             {
-              breakpoint: 1000,
-              settings: {
-                arrows: false,
-              }
+                breakpoint: 1000,
+                settings: {
+                    arrows: false,
+                }
             }
         ]
     });
 
+    // Инициализация Magnific Popup
+    $('.image-link').magnificPopup({
+        type: 'image',
+        gallery: {
+            enabled: true // Включаем галерею
+        },
+        callbacks: {
+            beforeOpen: function() {
+                // Устанавливаем текущий индекс для галереи
+                this.st.index = $('.image-link').index(this.st.el);
+            }
+        }
 
+    });
 });
